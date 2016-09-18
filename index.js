6e384db6ba0b5d740dbf8fefa241ba6c62c78db6
Guillaume Claret <dev@clarus.me>
2016-09-18 13:01:22 +0200

    Capture with the ISO time

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var url = 'https://www.momondo.fr/flightsearch?Search=true&TripType=2&SegNo=2&SO0=CDG&SD0=HND&SDP0=12-10-2016&SO1=HND&SD1=CDG&SDP1=14-10-2016&AD=1&TK=ECO&DO=false&NA=false';
+var url = 'https://www.momondo.fr/flightsearch?Search=true&TripType=2&SegNo=2&SO0=CDG&SD0=HND&SDP0=13-10-2016&SO1=HND&SD1=CDG&SDP1=14-10-2016&AD=1&TK=ECO&DO=false&NA=false';
 var page = require('webpage').create();
+
+function nowString() {
+  var currentDate = new Date();
+  return currentDate.toISOString();
+}
 
 page.open(url, function(status) {
   var title = page.evaluate(function() {
     return document.title;
   });
   console.log(title);
-  const waitDuration = 10;
+  var waitDuration = 10;
   console.log('Waiting', waitDuration, 'seconds...');
   if (status === "success") {
     setTimeout(function () {
-      page.render('screenshot.png');
+      var now = nowString();
+      page.render('screenshots/' + now + '.png');
       var firstPrice = page.evaluate(function() {
         var firstPriceElement = document.querySelector('.prices .price-pax .price .value');
         if (firstPriceElement) {
@@ -19,11 +25,7 @@ page.open(url, function(status) {
           return null;
         }
       });
-      if (firstPrice) {
-        console.log('First price:', firstPrice);
-      } else {
-        console.log('First price not found');
-      }
+      console.log(now, firstPrice ? firstPrice.replace(/\W/g, '') : '0');
       phantom.exit();
     }, waitDuration * 1000)
   }
